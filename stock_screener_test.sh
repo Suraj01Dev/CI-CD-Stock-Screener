18516d624b87ea449d4ee342866f5a0cdbf9c404
Suraj Dev <120789150+Suraj01Dev@users.noreply.github.com>
2024-03-30 11:45:35 +0530

    Update stock_screener_test.sh

--- stock_screener_test.sh
+++ stock_screener_test.sh
@@ -5,8 +5,4 @@ if [[ $? -ne 0 ]]; then
 fi
 
 pylint stock_screener -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt 
-if [[ $? -ne 0 ]]; then
-	exit 1
-fi
-
 exit 0
