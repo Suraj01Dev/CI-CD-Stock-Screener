acaabd64a39ad2ec67c424588d83b7edbc3f55ec
Suraj Dev <120789150+Suraj01Dev@users.noreply.github.com>
2024-03-30 11:43:34 +0530

    Update stock_screener_test.sh

--- stock_screener_test.sh
+++ stock_screener_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-bandit --format json --output bandit-report.json --recursive stock_app
+bandit --format json --output bandit-report.json --recursive stock_screener
 if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-pylint stock_app -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt 
+pylint stock_screener -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint-report.txt 
 if [[ $? -ne 0 ]]; then
 	exit 1
 fi
